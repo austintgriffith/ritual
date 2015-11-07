97f8c1bd96badc141f9de51145b11131bf53aaf5
atg <me@austingriffith.com>
2015-11-07 16:36:14 +0000

    start updates

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Building and running atg...";
+echo "Building and running brew...";
 git pull;
 git status;
 cd docker;
@@ -9,7 +9,42 @@ git pull;
 git status;
 cd docker
 ./build.sh
-./run.sh
+./run.sh 1
+./run.sh 2
+cd ..;
+cd ..;
+cd ..;
+
+echo "Building and running ontap...";
+cd ot
+git pull;
+git status;
+cd docker;
+./build.sh;
+cd ../src;
+git pull;
+git status;
+cd docker
+./build.sh
+./run.sh 1
+./run.sh 2
+cd ..;
+cd ..;
+cd ..;
+
+echo "Building and running link...";
+cd ln
+git pull;
+git status;
+cd docker;
+./build.sh;
+cd ../src;
+git pull;
+git status;
+cd docker
+./build.sh
+./run.sh 1
+./run.sh 2
 cd ..;
 cd ..;
 cd ..;
