72913431ef50e2e6708cf2fa101e8bb2f7567df1
atg <me@austingriffith.com>
2015-12-18 00:56:50 +0000

    webpack

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #remove existing container
-docker rm -f "ritual"
+docker rm -f "ritualwp"
 #run
-docker run --name="ritual" -p 50022:22 -d ritual
+docker run --name="ritualwp" -p 50022:22 -d ritualwp
